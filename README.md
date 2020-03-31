e61cec0dcb7759419288ecccd5315b55d749073a
user <user@example.com>
2020-04-01 01:09:26 +0800

    Added main logic

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # cangjie-tool
 A CangJie tool to handle cangjie code to / from traditional chinese 
+
+## Characteristics
+
+Support wildcard charactors:  
+-	* : 0 or any lenght of code
+-	? : 1 any code
+
+## Install
+
+```sh
+$ npm install cangjie-tool
+```
+
+## How to use
+
+```js
+const cangjie = require('cangjie-tool');
+
+cangjie.toWords('oiar');
+>>> { exact: true, words: [ '倉' ] }
+
+cangjie.toCode('頡');
+>>> { key: 'grmbc', cangjie: '土口一月金' }
+```
+
+## API
+
+### cangjie.toWords(code [, simplify = false [, exact = true]])
+
+Find traditional chinese words by cangjie code
+
+Parameters:
+
+-	code - (string) cangjie code in english
+-	simplify - (boolean) cangjie simplify mode (速成) (default: true)
+-	exact - (boolean) find only exact match to code (default: true)
+
+Return:
+
+-	exact - (boolean) result has exact match word
+-	words - (array[string]) result words
+
+### cangjie.toCode(word)
+
+Find traditional chinese words by cangjie code
+
+Parameters:
+
+-	code - (string) traditional chinese word
+
+Return:
+
+return `undefined` if fail to find the cangjie code.
+
+-	key - (string) cangjie code in english charactors
+-	cangjie - (string) cangjie code
---
+++ index.js
@@ -0,0 +1,135 @@
+'use strict';
+
+// import cangjie database
+const cdb = require('cangjie-db');
+
+// define module
+(() => {
+
+	/**
+	 * get regex
+	 */
+	const getRegex = (code, simpify, exact) => {
+
+		var hasWildcard = /^.*[\*\?].*$/.test(code);
+
+		if (hasWildcard) {
+
+			simpify = false;
+		}
+
+		// replace special charactor
+		code = code.split(/\*/).join('.{0,5}').split(/\?/).join('.{1}');
+
+		var regexExact = '^' + code + '$';
+
+		var regex = undefined;
+
+		if (exact) {
+
+			regex = regexExact;
+		} else if (code.length === 1 || !simpify) {
+
+			regex = '^' + code + (hasWildcard ? '' : '.*') + '$';
+		} else {
+
+			regex = '^' + code.substring(0, 1) + '.*' + code.substring(code.length - 1, code.length) + '$';
+		}
+
+		return {
+
+			regexExact: new RegExp(regexExact),
+
+			regex: new RegExp(regex)
+		};
+	};
+
+	/**
+	 * cangjie to word
+	 */
+	const toWords = (code, simpify = false, exact = false) => {
+
+		if (typeof code !== 'string' || !/^[a-z\*\?]{1,5}$/.test(code)) {
+
+			return [];
+		}
+
+		if (simpify && code.length > 2) {
+
+			return [];
+		}
+
+		var {
+
+			regexExact,
+
+			regex
+
+		} = getRegex(code, simpify, exact);
+
+		var foundExact = false;
+
+		var foundWords = cdb
+
+			.filter(r => {
+
+				foundExact || (regexExact.test(r.k) && (foundExact = true));
+
+				return regex.test(r.k);
+			})
+
+			.map(r => r.w);
+
+		return {
+
+			exact: foundExact,
+
+			words: [].concat(...foundWords)
+		};
+	};
+
+	/**
+	 * word to code
+	 */
+	const toCode = word => {
+
+		if (typeof word !== 'string') {
+
+			return undefined;
+		}
+
+		var results = cdb.filter(r => r.w.indexOf(word) !== -1);
+
+		if (results.length === 0) {
+
+			return undefined;
+		}
+
+		return {
+
+			key: results[0].k,
+
+			cangjie: results[0].k.split('').map(i => toWords(i, true).words[0]).join('')
+		};
+	};
+
+	// cangjie object
+	var cangjie = {
+
+		toWords,
+
+		toCode
+	};
+
+	// export methods
+	if (typeof module === 'object') {
+
+		module.exports = cangjie;
+	}
+
+	// browser
+	else if (typeof window === 'object') {
+
+		window.cangjie = cangjie;
+	}
+})();
